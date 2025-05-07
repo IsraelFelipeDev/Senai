56013e890aa979a8e66d4f17f8850f94ac9570fd
IsraelFelipeDev <felipealves6509@gmail.com>
2025-05-07 10:49:02 -0300

    Add files via upload

---
+++ senai_at1.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace at01
+{
+	class at01
+	{
+		static void Main()
+		{
+		int v1,v2,soma;
+		string nome;
+
+		Console.Write("Digite um valor: ");
+		v1=int.Parse(Console.ReadLine());
+		Console.WriteLine("Nome digitado:{0}",v1);
+		Console.ReadKey(true);
+		}
+	}
+}
